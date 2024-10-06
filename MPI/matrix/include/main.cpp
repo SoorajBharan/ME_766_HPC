21b0ef3a81651622f518427b9195b99eb4f38751
bharan <bharansooraj@gmail.com>
2024-10-06 22:14:15 +0530

    MPI->matrix multiplication implemented

--- MPI/matrix/include/main.cpp
+++ MPI/matrix/include/main.cpp
@@ -13,23 +13,94 @@ int main(int argc, char **argv)
 		std::cerr << "Usage : " << argv[0] << " <size_of_matrix> " << std::endl;
 		return 1;
 	}
-	uint mat_size = atoi(argv[1]);
+	MPI_Init(&argc, &argv);
+
+	const int mat_size = atoi(argv[1]);
 	double minimum = 0.0;
 	double maximum = 100.0;
+	const int root = 0;
+
+	int size, rank;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	int rows_per_core = mat_size / size;
+	int remaining_rows = mat_size % size;
+	int local_rows = (rank==size-1)?(rows_per_core+remaining_rows): (rows_per_core);
+
+	std::vector<double> global_A;
+	std::vector<double> global_B;
+	std::vector<double> global_B_transpose;
+	std::vector<double> global_C;
+	std::vector<double> local_A;
+	std::vector<double> local_C;
+	if(rank==0){
+		global_A.resize(mat_size*mat_size);
+		global_B.resize(mat_size*mat_size);
+		global_C.resize(mat_size*mat_size);
+	}
+	global_B_transpose.resize(mat_size*mat_size);
+	local_A.resize(local_rows*mat_size);
+	local_C.resize(local_rows*mat_size);
 
 	MatOperators::MatrixOperators<double> mat_operators;
-	std::vector<std::vector<double>> A(mat_size, std::vector<double>(mat_size));
-	std::vector<std::vector<double>> B(mat_size, std::vector<double>(mat_size));
-	std::vector<std::vector<double>> C(mat_size, std::vector<double>(mat_size));
+	std::mt19937 mt1(time(0)+rank);
 
-	std::mt19937 mt1(time(0));
-	std::mt19937 mt2(time(0)+1);
-	mat_operators.fill_matrix_random(A, mt1, minimum, maximum);
-	mat_operators.fill_matrix_random(B, mt2, minimum, maximum);
-	mat_operators.mat_x_mat(A, B, C);
+	mat_operators.fill_vectorised_matrix_random(local_A, mt1, minimum, maximum, local_rows, mat_size);
+	// Prepare to gather
+	std::vector<int> recvcounts(size, local_rows);
+	std::vector<int> displs(size);
 
-	auto end_time = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> elapse_time = end_time - start_time;
-	std::cout << "Wall time = " << elapse_time.count();
+	// Set recvcounts and displs
+	for (int i = 0; i < size; ++i) {
+		int rows = (i == size - 1) ? (rows_per_core + remaining_rows) : rows_per_core;
+		recvcounts[i] = rows * mat_size; // Total elements to receive from each process
+		displs[i] = (i == 0) ? 0 : displs[i - 1] + recvcounts[i - 1]; // Cumulative offset
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+	if(rank==0){
+		std::mt19937 mt2(time(0)+size);
+		mat_operators.fill_vectorised_matrix_random(global_B, mt2, minimum,  maximum, mat_size, mat_size);
+		global_B_transpose=mat_operators.transpose(global_B, mat_size, mat_size);
+	}
+	MPI_Bcast(global_B_transpose.data(), mat_size*mat_size, MPI_DOUBLE, root, MPI_COMM_WORLD);
+	mat_operators.mat_mult_transpose(local_A, global_B_transpose, local_C, local_rows, mat_size, mat_size);
+
+
+	// Gather the local matrices into the global matrix at the root
+	MPI_Gatherv(local_C.data(),
+			local_C.size(),
+			MPI_DOUBLE,
+			global_C.data(),
+			recvcounts.data(),
+			displs.data(),
+			MPI_DOUBLE,
+			root,
+			MPI_COMM_WORLD);
+
+	// Gather the local matrices into the global matrix at the root
+	MPI_Gatherv(local_A.data(),
+			local_A.size(),
+			MPI_DOUBLE,
+			global_A.data(),
+			recvcounts.data(),
+			displs.data(),
+			MPI_DOUBLE,
+			root,
+			MPI_COMM_WORLD);
+	if(rank==0){
+		std::cout << "\nMatrix A " << std::endl;
+		mat_operators.print_vector_matrix(global_A, mat_size, mat_size);
+		std::cout << "\nMatrix B " << std::endl;
+		mat_operators.print_vector_matrix(global_B, mat_size, mat_size);
+		std::cout << "\nMatrix B transpose " << std::endl;
+		mat_operators.print_vector_matrix(global_B_transpose, mat_size, mat_size);
+		std::cout << "\nMatrix C " << std::endl;
+		mat_operators.print_vector_matrix(global_C, mat_size, mat_size);
+		auto end_time = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> elapse_time = end_time - start_time;
+		std::cout << "\nWall time = " << elapse_time.count();
+	}
+	MPI_Finalize();
 	return 0;
 }
--- MPI/matrix/include/matrix_operators.h
+++ MPI/matrix/include/matrix_operators.h
@@ -18,13 +18,38 @@ public:
 			 	Number minimum,
 			  	Number maximum);
 
+	void fill_vectorised_matrix_random(std::vector<Number> & vector_in,
+				std::mt19937 mt,
+				Number minimum,
+				Number maximum,
+				int rows,
+				int cols);
+
 	void mat_x_mat(vectorised_matrix & mat_1,
 			vectorised_matrix & mat_2,
 			vectorised_matrix & mat_3);
 
-	void print_matrix(vectorised_matrix & mat);
+	void mat_mult(std::vector<Number> & mat_1,
+			std::vector<Number> & mat_2,
+			std::vector<Number> & mat_3,
+			int rows_1,
+			int cols_2,
+			int rows_2);
 
-	std::vector<std::vector<Number>> transpose(vectorised_matrix &);
+	void mat_mult_transpose(std::vector<Number> & mat_1,
+			std::vector<Number> & mat_2,
+			std::vector<Number> & mat_3,
+			int rows_1,
+			int cols_2,
+			int rows_2);
+	void print_matrix(vectorised_matrix & mat);
+	void print_vector_matrix(std::vector<Number> & mat,
+				int rows,
+				int cols);
+
+	std::vector<Number> transpose(std::vector<Number> &,
+					int rows,
+					int cols);
 };
 
 template<typename Number>
@@ -60,15 +85,48 @@ MatrixOperators<Number>::fill_matrix_random(vectorised_matrix & mat,
 }
 
 template<typename Number>
-std::vector<std::vector<Number>>
-MatrixOperators<Number>::transpose(vectorised_matrix & mat)
+void
+MatrixOperators<Number>::fill_vectorised_matrix_random(std::vector<Number> & vector_in,
+					    std::mt19937 mt,
+					    Number minimum,
+					    Number maximum,
+					    int rows,
+					    int cols)
 {
-	uint rows = mat.size();
-	uint cols = mat[0].size();
-	vectorised_matrix mat_t(cols, std::vector<Number>(rows));
+	if constexpr (std::is_floating_point<Number>::value){
+		std::uniform_real_distribution<Number> uni_distribution(minimum, maximum);
+		for(uint i=0; i < rows; ++i) {
+			for(uint j=0; j<cols; ++j){
+				vector_in[i*cols + j] = uni_distribution(mt);
+			}
+		}
+	}
+	else if constexpr (std::is_integral<Number>::value) {
+		std::uniform_int_distribution<Number> uni_distribution(minimum, maximum);
+		for(uint i=0; i < rows; ++i) {
+			for(uint j=0; j<cols; ++j){
+				vector_in[i*cols + j] = uni_distribution(mt);
+			}
+		}
+	}
+	else {
+		std::cerr << "Invalid datatype provided" << std::endl;
+		exit(0);
+	}
+
+	std::cout << "Vector initialised with random values" << std::endl;
+		
+}
+template<typename Number>
+std::vector<Number>
+MatrixOperators<Number>::transpose(std::vector<Number> & mat,
+				   int rows,
+				   int cols)
+{
+	std::vector<Number> mat_t(rows * cols);
 	for(uint i=0; i<rows; ++i){
 		for(uint j=0; j<cols; ++j){
-			mat_t[i][j]=mat[j][i];
+			mat_t[j*rows+i]=mat[i*cols+j];
 		}
 	}
 	std::cout << "Matrix transpose calculated" << std::endl;
@@ -110,6 +168,45 @@ MatrixOperators<Number>::mat_x_mat(vectorised_matrix & mat_1,
 
 template<typename Number>
 void
+MatrixOperators<Number>::mat_mult(std::vector<Number> & mat_1,
+				  std::vector<Number> & mat_2,
+				  std::vector<Number> & mat_3,
+				  int rows_1,
+				  int cols_2,
+				  int rows_2)
+{
+	for (int i=0; i<rows_1; ++i) {
+		for(int j=0; j<cols_2; ++j){
+			mat_3[i*cols_2 + j] = 0;
+			for(int k=0; k<rows_2; ++k){
+				mat_3[i*cols_2 + j] += mat_1[i*rows_2 + k] * mat_2[j*cols_2 + k];
+			}
+		}	
+	}
+
+}
+template<typename Number>
+void
+MatrixOperators<Number>::mat_mult_transpose(std::vector<Number> & mat_1,
+				  std::vector<Number> & mat_2,
+				  std::vector<Number> & mat_3,
+				  int rows_1,
+				  int cols_2,
+				  int rows_2)
+{
+	for (int i=0; i<rows_1; ++i) {
+		for(int j=0; j<cols_2; ++j){
+			mat_3[i*cols_2 + j] = 0.0;
+			for(int k=0; k<rows_2; ++k){
+				mat_3[i*cols_2 + j] += mat_1[i*rows_2 + k] * mat_2[j*rows_2 + k];
+			}
+		}	
+	}
+
+}
+
+template<typename Number>
+void
 MatrixOperators<Number>::print_matrix(vectorised_matrix & mat)
 {
 	for(uint i=0; i<mat.size(); ++i){
@@ -119,6 +216,20 @@ MatrixOperators<Number>::print_matrix(vectorised_matrix & mat)
 		}
 	}
 	std::cout << std::endl;
+}
+
+template<typename Number>
+void
+MatrixOperators<Number>::print_vector_matrix(std::vector<Number> & mat,
+					     int rows,
+					     int cols)
+{
+	for(int i=0; i<rows; ++i){
+		std::cout << std::endl;
+		for(int j=0; j<cols; ++j){
+			std::cout << '\t' << mat[i*cols + j];
+		}
+	}
 }
 
 } // namespace MatOperators
